5ce3801badad1b9e2dae7942ad78a41a55fd8a53
arushiCoder01 <arrushi.sharrma@gmail.com>
2020-11-23 15:08:33 +0530

    added platfoem

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform = new Ground(130,200,300,80);    
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -45,6 +45,9 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+
+    platform.display();
+    
     ground.display();
     pig1.display();
     log1.display();
